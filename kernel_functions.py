0d63e2d1a3195cfba0a79d85a3f84a8ab3adc603
Josef Malmström <josef.malmstrom@gmail.com>
2019-12-18 15:19:53 +0100

    added basic cluster kernel

--- kernel_functions.py
+++ kernel_functions.py
@@ -41,9 +41,9 @@ def kernel_step(Lambda, k):
     return Lambda_tilde
 
 
-def kernel_linear_step(Lambda):
-    Lambda_ind = Lambda.argsort()[-2:][::-1]
-    lambda_cut = Lambda[Lambda_ind[1]]
+def kernel_linear_step(Lambda, k):
+    Lambda_ind = Lambda.argsort()[-k:][::-1]
+    lambda_cut = Lambda[Lambda_ind[k - 1]]
 
     Lambda_tilde = np.zeros(Lambda.shape)
     for i in range(len(Lambda)):
@@ -61,17 +61,17 @@ def kernel_poly(Lambda, t):
 
 
 def kernel_poly_step(Lambda, r, p, q):
-    Lambda[::-1].sort()
+    # Lambda[::-1].sort()
     Lambda_tilde = np.zeros(Lambda.shape)
     for i in range(len(Lambda)):
         if i <= r:
-            Lambda_tilde[i] = np.power(Lambda_tilde[i], p)
+            Lambda_tilde[i] = np.power(Lambda[i], p)
         else:
-            Lambda_tilde[i] = np.power(Lambda_tilde[i], q)
+            Lambda_tilde[i] = np.power(Lambda[i], q)
     return Lambda_tilde
 
 
-def clustring_kernel(X, gamma, k):
+def clustring_kernel(X_train, X_unlabeled, X_test, gamma, k):
     """
     :param X: The complete data point
     :param gamma: RBF parameter
@@ -79,22 +79,22 @@ def clustring_kernel(X, gamma, k):
     :return: The kernel phi(X)
     """
 
+    # Concatenate data
+    X = np.concatenate((X_train, X_test, X_unlabeled))
+
     # affitnity matrix
     # Set diagonal to zero
-    print("passed 1")
     K = rbf_kernel(X)
-    print("passedd 2")
     np.fill_diagonal(K, 0)
 
     # D matrix
-    D = np.zeros(K.shape, int)
+    sqrt_D = np.zeros(K.shape)
     for i, row in enumerate(K):
         row_sum = np.sum(row)
-        D[i, i] = row_sum
+        sqrt_D[i, i] = np.power(row_sum, -0.5)
 
     # matrix L
-    sqrt_D = np.power(D, (-0.5))
-    L = np.dot(sqrt_D, K).dot(sqrt_D)
+    L = np.matmul(sqrt_D, np.matmul(K, sqrt_D))
 
     _, V = np.linalg.eig(L)
     # take only k eigenvectors
@@ -110,7 +110,9 @@ def clustring_kernel(X, gamma, k):
         for j in range(k):
             V[i, j] = V[i, j] / np.sqrt(S[i])
 
-    return V
+    V_train, V_test = V[0:X_train.shape[0], :], V[X_train.shape[0]:X_train.shape[0] + X_test.shape[0], :]
+
+    return V_train, V_test
 
 
 def cluster_kernel_extension(X_labeled, X_unlabeled, X_test, gamma, kernel, t):
@@ -124,7 +126,10 @@ def cluster_kernel_extension(X_labeled, X_unlabeled, X_test, gamma, kernel, t):
     :return: The The kernel phi(X)
 
     """
-    X = np.concatenate((X_labeled, X_test, X_unlabeled))
+    if X_test is not None:
+        X = np.concatenate((X_labeled, X_test, X_unlabeled))
+    else:
+        X = np.concatenate((X_labeled, X_unlabeled))
 
     print("Cluster kernel start")
 
@@ -153,14 +158,13 @@ def cluster_kernel_extension(X_labeled, X_unlabeled, X_test, gamma, kernel, t):
     elif kernel == "step":
         Lambda_tilde = kernel_step(Lambda, k=50)
     elif kernel == "lin_step":
-        Lambda_tilde = kernel_linear_step(Lambda)
+        Lambda_tilde = kernel_linear_step(Lambda, k=50)
     elif kernel == "poly":
-        Lambda_tilde = kernel_poly(Lambda, t)
+        Lambda_tilde = kernel_poly(Lambda, t=2)
     elif kernel == "poly_step":
-        Lambda_tilde = kernel_poly_step(Lambda, r=50, p=2, q=2)
+        Lambda_tilde = kernel_poly_step(Lambda, r=10, p=2, q=2)
 
     print("number cluster k :", np.count_nonzero(Lambda_tilde))
-    # L_tilde = U * Lambda_tilde * U.T
     L_tilde = np.matmul(U * Lambda_tilde, U.T)
 
     # compute D_tilde
@@ -175,16 +179,12 @@ def cluster_kernel_extension(X_labeled, X_unlabeled, X_test, gamma, kernel, t):
     # Filter K_tilde just for the labeled data set.
     K_tilde_labeled = K_tilde[0:X_labeled.shape[0], 0:X_labeled.shape[0]]
 
-    K_tilde_test = K_tilde[X_labeled.shape[0]:X_labeled.shape[0] + X_test.shape[0], 0:X_labeled.shape[0]]
-
-    K_tilde_unlabeled = K_tilde[X_labeled.shape[0] + X_unlabeled.shape[0]:
-                                X_labeled.shape[0] + X_unlabeled.shape[0] + X_test.shape[0],
-                                X_labeled.shape[0] + X_unlabeled.shape[0]:
-                                X_labeled.shape[0] + X_unlabeled.shape[0] + X_test.shape[0]]
+    if X_test is not None:
+        K_tilde_test = K_tilde[X_labeled.shape[0]:X_labeled.shape[0] + X_test.shape[0], 0:X_labeled.shape[0]]
 
     print("passed the cluster kernels")
 
-    print(K_tilde_labeled)
-    print(K_tilde_test)
-
-    return K_tilde_labeled, K_tilde_unlabeled, K_tilde_test
+    if X_test is not None:
+        return K_tilde_labeled, K_tilde_test
+    else:
+        return K_tilde_labeled, K[0:X_labeled.shape[0], 0:X_labeled.shape[0]]
--- train_svm.py
+++ train_svm.py
@@ -4,6 +4,8 @@ import os
 import numpy as np
 from sklearn.svm import SVC
 from sklearn import metrics
+from sklearn.metrics.pairwise import rbf_kernel
+
 from kernel_functions import *
 
 np.random.seed(8)
@@ -11,6 +13,7 @@ np.random.seed(8)
 # Hyperparameters
 C = 3.0
 kernel = "rbf"
+GAMMA = 1 / (2 * 5 ** 2)
 
 
 def compute_accuracy(predictions, targets):
@@ -20,13 +23,24 @@ def compute_accuracy(predictions, targets):
     return predictions[predictions == targets].size / predictions.size
 
 
+def compute_K_test(K_tilde_labeled, K_labeled, train_inputs, test_inputs, GAMMA):
+    """
+    Compute kernel of test points as linear combination of training points.
+    """
+    V = rbf_kernel(test_inputs, train_inputs, GAMMA=GAMMA)
+    K_tilde_test = np.matmul(K_tilde_labeled, np.matmul(np.linalg.inv(K_labeled), V.T)).T
+
+    return K_tilde_test
+
+
 def main():
 
     # Argument parsing
     argparser = argparse.ArgumentParser()
     argparser.add_argument("load_path", type=str, help="Path to load data from.")
     argparser.add_argument("type_kernel", type=str, help="Specify the type of kernel used in SVM. "
-                                                         "('normal', 'linear','step','lin_step','kernel_poly')")
+                                                         "('normal', 'basic_cluster, ''linear','step','lin_step','poly', 'poly_step')")
+    argparser.add_argument("--test_points_at_train", type=int, default=1, help="Specify whether to use test points at training time (1) or not (0)")
     args = argparser.parse_args()
 
     # Load data
@@ -66,14 +80,24 @@ def main():
     test_targets = test_targets[random_perm]
 
     # Run SVM
-    if args.type_kernel == "normal":
+    if args.type_kernel == "normal":  # normal version
         svm = SVC(C=C, kernel="rbf")
         svm.fit(train_inputs, train_targets)
-    else:
-        gamma = 1 / (2 * 5 ** 2)
-        K_labeled, K_unlabeled, K_test = cluster_kernel_extension(train_inputs, train_unlabeled[0:40, :], test_inputs, gamma, args.type_kernel, 1)
+
+    elif args.type_kernel == "basic_cluster":  # basic cluster kernel version
+        svm = SVC(C=C, kernel="rbf")
+        train_inputs_modified, test_inputs_modified = clustring_kernel(train_inputs, train_unlabeled[0:40, :], test_inputs, GAMMA, k=2)
+        svm.fit(train_inputs_modified, train_targets)
+
+    else:  # extended cluster kernel version
+
+        if args.test_points_at_train == 1:
+            K_tilde_labeled, K_test = cluster_kernel_extension(train_inputs, train_unlabeled[0:40, :], test_inputs, GAMMA, args.type_kernel, 1)
+        else:
+            K_tilde_labeled, K_labeled = cluster_kernel_extension(train_inputs, train_unlabeled[0:40, :], None, GAMMA, args.type_kernel, 1)
+
         svm = SVC(C=C, kernel="precomputed")
-        svm.fit(K_labeled.T, train_targets.T)
+        svm.fit(K_tilde_labeled.T, train_targets.T)
 
     print("train_input shape:", train_inputs.shape)
     print("test_input shape:", test_inputs.shape)
@@ -81,7 +105,11 @@ def main():
     # Test SVM
     if args.type_kernel == "normal":
         test_predictions = svm.predict(test_inputs)
+    elif args.type_kernel == "basic_cluster":
+        test_predictions = svm.predict(test_inputs_modified)
     else:
+        if args.test_points_at_train == 0:
+            K_test = compute_K_test(K_tilde_labeled, K_labeled, train_inputs, test_inputs, GAMMA)
         test_predictions = svm.predict(K_test)
 
     # performance
